f379102e952f9f46dcfbfba93154cde7e90a73fd
JungInRhee <ysrheee@kaist.ac.kr>
2021-09-01 15:03:47 +0900

    feat: cron job available

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,11 +1,15 @@
 import expressLoader from "./express";
 import ormLoader from "./typeorm";
+import cronLoader from "./cron";
 import Logger from "./logger";
 
 export default async ({ expressApp }) => {
   Logger.info("Load start");
   await ormLoader();
   Logger.info("Orm loaded");
+
+  cronLoader();
+  Logger.info("Cron Loaded");
 
   expressLoader({ app: expressApp });
   Logger.info("Express loaded");
