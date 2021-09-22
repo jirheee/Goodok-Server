d4ca6cf4fe820327403c75f76befd162064e4160
JungInRhee <ysrheee@kaist.ac.kr>
2021-09-23 01:00:59 +0900

    feat: update seeding script to handle more concrete scenarios

--- src/jobs/seed/seed.ts
+++ src/jobs/seed/seed.ts
@@ -1,5 +1,9 @@
+import user from "@/api/routes/user";
 import config from "@/config";
+import { Subscribe } from "@/db/entity/subscribe";
 import { Website } from "@/db/entity/website";
+import { SubscribeRepository } from "@/db/repository/SubscribeRepository";
+import { UserRepository } from "@/db/repository/UserRepository";
 import { WebsiteRepository } from "@/db/repository/WebsiteRepository";
 import { createConnection } from "typeorm";
 
@@ -12,17 +16,50 @@ const urls = [
   "https://news.naver.com/main/list.naver?mode=LSD&mid=sec&sid1=102"
 ];
 
+const users: { email: string; password: string }[] = [
+  {
+    email: "user1",
+    password: "1234"
+  }
+];
+
+async function dropDatabase() {
+  console.log(
+    "******* This will drop the whole database. Will you precede? *******"
+  );
+
+  // TODO: get imput to precede or not
+
+  const connection = await createConnection(config.db);
+  await connection.dropDatabase();
+  await connection.close();
+}
+
 async function seed() {
   const connection = await createConnection(config.db);
   const websiteRepository = connection.getCustomRepository(WebsiteRepository);
+  const userRepository = connection.getCustomRepository(UserRepository);
+  const subscribeRepository =
+    connection.getCustomRepository(SubscribeRepository);
+
+  const userData = await Promise.all(
+    users.map(user => userRepository.saveUser(user.email, user.password))
+  );
 
   for (const url of urls) {
     const website = new Website();
     website.domain = url;
-    await websiteRepository.saveWebsite(website);
+    const savedWebsite = await websiteRepository.saveWebsite(website);
+
+    const subscribe = await subscribeRepository.createSubscribe(
+      url,
+      "Red",
+      userData[0],
+      savedWebsite
+    );
   }
 
   console.log("Finished Seeding");
 }
 
-seed();
+dropDatabase().then(() => seed());
