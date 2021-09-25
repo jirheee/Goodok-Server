1991b005877318ed6ba9c8a4a7a2d0ba0a174586
JUNJAJAJA <whwns888@kaist.ac.kr>
2021-09-25 21:56:57 +0900

    feat:set user api frame

--- src/api/routes/user.ts
+++ src/api/routes/user.ts
@@ -1,7 +1,26 @@
 import { Router } from "express";
+import { getCustomRepository } from "typeorm";
 
 const route = Router();
 
 export default (app: Router) => {
-  app.use("users", route);
+  app.use("/user", route);
+  const authService: AuthService = new AuthService();
+  const userService: UserService = new UserService(
+    getCustomRepository(UserRepository)
+  );
+  route.post("/login", userService.login, authService.newAccessToken);
+  route.post("/", userService.signup);
+  route.put(
+    "/profile",
+    authService.checkAccessToken,
+    userService.changeProfile,
+    authService.newAccessToken
+  );
+  route.put(
+    "/search_list",
+    authService.checkAccessToken,
+    userService.changeSearch
+  );
+  route.delete("/", authService.checkAccessToken, userService.deleteUser);
 };
